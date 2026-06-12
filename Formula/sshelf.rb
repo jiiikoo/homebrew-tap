016b8f6f322e2c962736fd0f6f5319c69c5c5e19
jiiikoo <31279180+jiiikoo@users.noreply.github.com>
2026-06-12 13:13:15 +0300

    Update sshelf.rb

--- Formula/sshelf.rb
+++ Formula/sshelf.rb
@@ -1,7 +1,7 @@
 class Sshelf < Formula
   desc "Fast terminal UI for managing and connecting to SSH hosts"
   homepage "https://github.com/jiiikoo/sshelf"
-  url "https://github.com/jeskarja/sshelf/archive/refs/tags/v0.2.0-jesper.tar.gz"
+  url "https://github.com/jiiikoo/sshelf/archive/refs/tags/v0.2.0-jiiikoo.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
